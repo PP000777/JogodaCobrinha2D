a083450593a9b27892ab660c366a3dcebf14d5fc
PP000777 <bia.silva007@hotmail.com>
2025-03-24 09:50:28 -0300

    colocando sistema de pontuação

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const canvas = document.getElementById('jogoCanvas')
-const ctx = canvas.getContext('2d')
-let rodando = true
-let gameOverr = false
+const canvas = document.getElementById('jogoCanvas');
+const ctx = canvas.getContext('2d');
+const pontuacaoElement = document.getElementById('pontuacao');
+const pontuacaoMaximaElement = document.getElementById('pontuacaomax');
+
+let rodando = true;
+let gameOverr = false;
+let pontuacao = 0;
+let pontuacaoMaxima = localStorage.getItem('pontuacaoMaxima') || 0;
+
+pontuacaoMaximaElement.textContent = `Pontuação Máxima = ${pontuacaoMaxima}`;
 
 const teclasPressionadas = {
     KeyW: false,
@@ -9,6 +16,7 @@ const teclasPressionadas = {
     KeyD: false,
     KeyA: false
 };
+
 document.addEventListener('keydown', (e) => {
     for (let tecla in teclasPressionadas) {
         if (teclasPressionadas.hasOwnProperty(e.code)) {
@@ -20,38 +28,39 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
-
 class Entidade {
-    constructor(x, y, largura, altura,cor) {
-        this.x = x
-        this.y = y
-        this.largura = largura
-        this.altura = altura
-        this.cor = cor
+    constructor(x, y, largura, altura, cor) {
+        this.x = x;
+        this.y = y;
+        this.largura = largura;
+        this.altura = altura;
+        this.cor = cor;
     }
+    
     desenhar() {
-        ctx.fillStyle = this.cor
-        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+        ctx.fillStyle = this.cor;
+        ctx.fillRect(this.x, this.y, this.largura, this.altura);
     }
 }
 
-
 class Cobra extends Entidade {
-    #cor
-    constructor(x, y, largura, altura,cor) {
-        super(x, y, largura, altura,cor)
+    #cor;
+    constructor(x, y, largura, altura, cor) {
+        super(x, y, largura, altura, cor);
     }
+    
     atualizar() {
         if (teclasPressionadas.KeyW) {
-            this.y -= 7
+            this.y -= 7;
         } else if (teclasPressionadas.KeyS) {
-            this.y += 7
+            this.y += 7;
         } else if (teclasPressionadas.KeyA) {
-            this.x -= 7
+            this.x -= 7;
         } else if (teclasPressionadas.KeyD) {
-            this.x += 7
+            this.x += 7;
         }
     }
+    
     verificarColisao(comida) {
         if (
             this.x < comida.x + comida.largura &&
@@ -59,52 +68,66 @@ class Cobra extends Entidade {
             this.y < comida.y + comida.altura &&
             this.y + this.altura > comida.y
         ) {
-            this.#houveColisao(comida)
+            this.#houveColisao(comida);
         }
     }
+    
     #houveColisao(comida) {
-        comida.x = Math.random() * canvas.width - 10
-        comida.y = Math.random() * canvas.height - 10
+        comida.x = Math.random() * (canvas.width - 20);
+        comida.y = Math.random() * (canvas.height - 20);
+        pontuacao++;
+        pontuacaoElement.textContent = `Pontuação = ${pontuacao}`;
+        
+        if (pontuacao > pontuacaoMaxima) {
+            pontuacaoMaxima = pontuacao;
+            pontuacaoMaximaElement.textContent = 'Pontuação Máxima = ${pontuacaoMaxima}';
+            localStorage.setItem('pontuacaoMaxima', pontuacaoMaxima);
+        }
     }
 }
+
 class Comida extends Entidade {
     constructor(cor) {
-        super(Math.random() * canvas.width - 10, Math.random() * canvas.height - 10, 20, 20, cor)
+        super( Math.random() * (canvas.width - 20),Math.random() * (canvas.height - 20),20,20, cor);
     }
 }
 
 function gameOver() {
-    if(gameOverr = true){
-    alert('Game Over!')
-    location.reload()
-}
-}
-
-function verificarcolisaoParede(){
-    if (cobra.x < 0 || cobra.x > canvas.width - cobra.largura || cobra.y < 0 || cobra.y > canvas.height - cobra.altura) {
-        gameOver()
-        rodando = false
-        gameOverr = true
+    if (gameOverr) {
+        alert('Game Over! Sua pontuação: ${pontuacao}');
+        location.reload();
     }
 }
 
+function verificarcolisaoParede() {
+    if (
+        cobra.x < 0 ||
+        cobra.x > canvas.width - cobra.largura ||
+        cobra.y < 0 ||
+        cobra.y > canvas.height - cobra.altura
+    ) {
+        gameOverr = true;
+        gameOver();
+        rodando = false;
+    }
+}
 
-const cobra = new Cobra(100, 200, 20, 20, 'green')
-const comida = new Comida('red')
-
+const cobra = new Cobra(100, 200, 20, 20, 'green');
+const comida = new Comida('red');
 
 function loop() {
-    if(!rodando){
+    if (!rodando) {
         return;
     }
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    cobra.desenhar(ctx, cobra.cor)
-    cobra.atualizar()
-    comida.desenhar(ctx, comida.cor)
-    cobra.verificarColisao(comida)
-    verificarcolisaoParede()
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cobra.desenhar();
+    cobra.atualizar();
+    comida.desenhar();
+    cobra.verificarColisao(comida);
+    verificarcolisaoParede();
     
-    requestAnimationFrame(loop)
+    requestAnimationFrame(loop);
 }
-loop()
+
+loop();
